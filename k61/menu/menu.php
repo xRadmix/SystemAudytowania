a207d271e7cd3ce23205eeebc3c86ef41d571b6c
Vertuz <jw.pozniak@gmail.com>
2023-05-26 20:05:55 +0200

    aktualizacja dostępu dla admina

--- k61/menu/menu.php
+++ k61/menu/menu.php
@@ -88,19 +88,15 @@
                 <p>Generowanie raportów</p>
             </div>
         </a>
-        <?php
-#SPRAWDZAMY CZY UŻYTKOWNIK TO ADMIN
-
-    if($_SESSION['login']=="Admin")
-    {
-       <a href="admin/show.php">
+        <?php if($_SESSION['login']=="Admin") : ?>
+    <a href="admin/show.php">
             <div class=choice id=d style="background-color: #0B6FA4">
                 <i class="fa fa-lock fa-5x"></i>
                 <p>Panel administratora</p>
             </div>
         </a>
-    }
-?>	
+<?php endif; ?>
+
     </div>
     <!--end of strona-->
 </body>
